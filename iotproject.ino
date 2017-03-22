3ef639d440985b69c481761af9c57402dcfa48c4
Dylan Maxwell <s00171073@e1010-18.local>
2017-03-22 14:50:09 +0000

    iotproject

---
+++ iotproject.ino
@@ -0,0 +1,27 @@
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int buzPin =  4.;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(buzPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(buzPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(buzPin, LOW);
+  }
+}
